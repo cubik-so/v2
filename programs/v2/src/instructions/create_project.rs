9f93825025f77ca155eef5aa50b735a387912018
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-11-17 22:49:33 +0530

    feat: squad changes

--- programs/v2/src/instructions/create_project.rs
+++ programs/v2/src/instructions/create_project.rs
@@ -2,7 +2,7 @@ use crate::event::NewProject;
 use crate::state::{user, Admin, Project, ProjectVerification, User};
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::{self, system_program, sysvar::rent::Rent};
-use squads_multisig_program::{self, MultisigCreate};
+use squads_multisig_program::{self, cpi, MultisigCreate};
 
 #[derive(Accounts)]
 #[instruction(counter:u64)]
@@ -30,6 +30,8 @@ pub struct CreateProjectContext<'info> {
     )]
     pub user_account: Box<Account<'info, User>>,
 
+    #[account(address = squads_multisig_program::ID)]
+    pub squads_program: Program<'info, squads_multisig_program::program::SquadsMultisigProgram>,
     // Misc Accounts
     #[account(address = system_program::ID)]
     pub system_program: Program<'info, System>,
@@ -42,20 +44,24 @@ pub fn handler(
     counter: u64,
     multi_sig: Pubkey,
     metadata: String,
+    multisig_args: squads_multisig_program::MultisigCreateArgs,
 ) -> Result<()> {
     let project_account = &mut ctx.accounts.project_account;
     let user_account = &mut ctx.accounts.user_account;
 
-    // squads_multisig_program::cpi::multisig_create(
-    //     ctx,
-    //     squads_multisig_program::MultisigCreateArgs {
-    //         members,
-    //         threshold,
-    //         config_authority,
-    //         time_lock,
-    //         memo,
-    //     },
-    // );
+    let create_multisig = squads_multisig_program::cpi::accounts::MultisigCreate {
+        create_key: user_account.to_account_info(), // this is example
+        creator: user_account.to_account_info(),    // this is example
+        multisig: user_account.to_account_info(),
+        system_program: ctx.accounts.system_program.to_account_info(),
+    };
+
+    let cpi_ctx_squads = CpiContext::new(
+        ctx.accounts.squads_program.to_account_info(),
+        create_multisig,
+    );
+
+    squads_multisig_program::cpi::multisig_create(cpi_ctx_squads, multisig_args);
 
     project_account.owner = user_account.authority.key();
     project_account.status = ProjectVerification::UnderReview;
