5f54b162497ca3f33d3d7bed416c84bf4bbf46bb
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-12-04 10:39:03 +0530

    chores: refactor

--- sdk/.solitarc.js
+++ sdk/.solitarc.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const programDir = path.join(__dirname, '..', 'programs', 'v2');
+const programDir = path.join(__dirname, '..', 'programs', 'cubik');
 const idlDir = path.join(__dirname, 'idl');
 const sdkDir = path.join(__dirname, 'src', 'generated');
 const binaryInstallDir = path.join(__dirname, '.crates');
