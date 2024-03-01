56f5a0dcc55bec700f5077682fcb69226fc537db
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2024-03-02 00:59:24 +0530

    updated changes to mainnet

--- programs/cubik/src/lib.rs
+++ programs/cubik/src/lib.rs
@@ -7,8 +7,8 @@ use instructions::*;
 pub mod state;
 use state::*;
 
-declare_id!("GsKHdvSBodD3ZGMAMWZ6sSwNZAixsE6XZ4xKA4KyEwc");
-// declare_id!("CUbkXMRWxumGzDwf43ysyFm3da77JRuUqLF1bmW4tGoZ");
+// declare_id!("GsKHdvSBodD3ZGMAMWZ6sSwNZAixsE6XZ4xKA4KyEwc");
+declare_id!("CUbkXMRWxumGzDwf43ysyFm3da77JRuUqLF1bmW4tGoZ");
 
 #[program]
 pub mod cubik {
