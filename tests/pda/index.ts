a156bcc36b31e36a0e29a4e7db9c81ff89a4a518
satendra <ersatendrasingh1325@gmail.com>
2024-05-27 23:54:34 +0530

    fix: added event idel test case

--- tests/pda/index.ts
+++ tests/pda/index.ts
@@ -1,10 +1,65 @@
 import { web3 } from "@coral-xyz/anchor";
-import { PROGRAM_ID } from "../utils";
+import {
+  PROGRAM_ID,
+  SEED_MULTISIG,
+  SEED_PREFIX,
+  SQUADS_PROGRAM_ID,
+} from "../utils";
 import { BN } from "bn.js";
 
 export const getProjectPDA = (createKey: web3.PublicKey) => {
   return web3.PublicKey.findProgramAddressSync(
     [Buffer.from("project"), createKey.toBuffer()],
+    PROGRAM_ID
+  );
+};
+
+export const getMultisigPDA = (createKey: web3.PublicKey) => {
+  return web3.PublicKey.findProgramAddressSync(
+    [SEED_PREFIX, SEED_MULTISIG, createKey.toBytes()],
+    SQUADS_PROGRAM_ID
+  );
+};
+
+export const getEventPDA = (createKey: web3.PublicKey) => {
+  return web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("event"), createKey.toBuffer()],
+    PROGRAM_ID
+  );
+};
+
+export const getEventTeamPDA = (
+  eventAccount: web3.PublicKey,
+  authority: web3.PublicKey
+) => {
+  return web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("event"),
+      eventAccount.toBuffer(),
+      Buffer.from("team"),
+      authority.toBuffer(),
+    ],
+    PROGRAM_ID
+  );
+};
+
+export const getEventParticipantPDA = (
+  eventAccount: web3.PublicKey,
+  projectAccount: web3.PublicKey
+) => {
+  return web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("eventParticipant"),
+      eventAccount.toBuffer(),
+      projectAccount.toBuffer(),
+    ],
+    PROGRAM_ID
+  );
+};
+
+export const getTeamPDA = (createKey: web3.PublicKey) => {
+  return web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("team"), createKey.toBuffer()],
     PROGRAM_ID
   );
 };
